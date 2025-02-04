ab764e200651dc330d97884b9c59d95191d0f291
Ahtisham ul Haq <142933781+AhtishamDeveloper09@users.noreply.github.com>
2025-02-04 18:41:07 +0500

    file name changed

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { ThreeCircles } from "react-loader-spinner";
+
+const Loader = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+  return (
+    <div className="absolute z-[900] inset-0 h-screen w-full flex justify-center items-center bg-darkBgColor">
+      <ThreeCircles
+        visible={true}
+        height="100"
+        width="100"
+        color="#975DE7"
+        ariaLabel="three-circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
+
+export default Loader;
